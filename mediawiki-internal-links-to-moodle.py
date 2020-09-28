d8cdc6a2d7ab55e28d6087d1b82087ae1b6b2fad
Moritz 'e1mo' Fromm <git@e1mo.de>
2020-09-28 16:36:12 +0200

    Add filter for preserving mediawiki wikilinks

---
+++ mediawiki-internal-links-to-moodle.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+
+"""
+Pandoc filter to convert CodiMDs [name=] tags
+to dokuwiki links
+"""
+
+from panflute import *
+import re
+
+nametag_re = r"\[name=([a-zA-Z0-9]+)\]"
+
+def replace_wikilinks(elem, doc):
+    if isinstance(elem, Link):
+        debug(elem.content)
+        if elem.title == 'wikilink':
+            debug(stringify(elem))
+
+            return Str('[['+stringify(elem)+']]')
+
+def main(doc=None):
+   return run_filter(replace_wikilinks, doc=doc) 
+
+if __name__ ==  '__main__':
+    main()
+    
